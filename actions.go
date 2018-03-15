f6bcf04810721b5d9a43bcddefd61bc7f212cd13
Havarem <andre.jacques@nexopixo.com>
2018-03-14 20:28:53 -0400

    Adds comments and Git Ignore
    Because comment is important.

--- actions.go
+++ actions.go
@@ -5,16 +5,19 @@ import (
 	"fmt"
 )
 
+// Handles the init command of the machconf tool
 func InitAction(c *cli.Context) error {
 	fmt.Println("Init not yet implemented")
 	return nil
 }
 
+// Handles the configure command of the machconf tool
 func ConfigureAction(c *cli.Context) error {
 	fmt.Println("Configure not yet implemented")
 	return nil
 }
 
+// Handles the tag command of the machconf tool
 func TagAction(c *cli.Context) error {
 	fmt.Println("Tag not yet implemented")
 	return nil
--- commands.go
+++ commands.go
@@ -3,17 +3,20 @@ package main
 import "github.com/urfave/cli"
 
 var (
+	// Init command definition
 	InitCmd = cli.Command {
 		Name: "init",
 		Usage: "Initialize a machine configuration file",
 		Action: InitAction,
 	}
+	// Configure command definition
 	ConfigureCmd = cli.Command {
 		Name: "configure",
 		Usage: "Configure the machine",
 		Flags: []cli.Flag { FqdnFlag, Ip4Flag, TagsFlag },
 		Action: ConfigureAction,
 	}
+	// Tag command definition
 	TagCmd = cli.Command {
 		Name: "tag",
 		Usage: "Manages machine configuration tags",
