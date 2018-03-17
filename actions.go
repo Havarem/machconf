192c23814e36fb64d02773b6f38a5ab57a065f77
Havarem <andre.jacques@nexopixo.com>
2018-03-17 10:20:13 -0400

    MVP completed
    We can now add and remove tags, and a tag can't be set twice.

--- actions.go
+++ actions.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/urfave/cli"
 
@@ -35,7 +35,7 @@ func InitAction(c *cli.Context) error {
 		machine := Machine {
 			Ip: nil,
 			Domain: "",
-			Flags: []Flag { },
+			Tags: []Tag { },
 		}
 
 		err = writeConfig(machine)
@@ -67,6 +67,31 @@ func openConfig() (Machine, error) {
 	return result, err
 }
 
+func unique(tags []Tag) []Tag {
+	u := make([]Tag, 0, len(tags))
+	m := make(map[Tag]bool)
+
+	for _, val := range tags {
+		if _, ok := m[val]; !ok {
+			m[val] = true
+			u = append(u, val)
+		}
+	}
+
+	return u
+}
+
+func separateTags(tags string) []Tag {
+	var result []Tag
+	resultString := strings.Split(tags, ",")
+
+	for i := 0; i < len(resultString); i++ {
+		result = append(result, Tag(resultString[i]))
+	}
+
+	return result
+}
+
 // Handles the configure command of the machconf tool
 func ConfigureAction(c *cli.Context) error {
 	machine, err := openConfig()
@@ -76,8 +101,16 @@ func ConfigureAction(c *cli.Context) error {
 		machine.Domain = c.String("fqdn")
 	}
 
-	if c.String("ip") != "" {
-		machine.Ip = net.ParseIP(c.String("ip"))
+	if c.String("ip4") != "" {
+		machine.Ip = net.ParseIP(c.String("ip4"))
+	}
+
+	if c.String("add-tags") != "" {
+		addTag := separateTags(c.String("add-tags"))
+
+		for i := 0; i < len(addTag); i++ {
+			machine.Tags = unique(append(machine.Tags, addTag[i]))
+		}
 	}
 
 	err = writeConfig(machine)
@@ -88,8 +121,43 @@ func ConfigureAction(c *cli.Context) error {
 	return nil
 }
 
+func difference(current []Tag, removed []Tag) []Tag {
+	diffStr := []Tag {}
+	m := map [Tag] int{}
+
+	for _, s1Val := range current {
+		m[s1Val] = 1
+	}
+
+	for _, s2Val := range removed {
+		m[s2Val] = m[s2Val] + 1
+	}
+
+	for mKey, mVal := range m {
+		if mVal == 1 {
+			diffStr = append(diffStr, mKey)
+		}
+	}
+
+	return diffStr
+}
+
 // Handles the tag command of the machconf tool
 func TagAction(c *cli.Context) error {
-	fmt.Println("Tag not yet implemented")
+	machine, err := openConfig()
+	check(err)
+
+	if c.String("add-tags") != "" {
+		addTag := separateTags(c.String("add-tags"))
+
+		for i := 0; i < len(addTag); i++ {
+			machine.Tags = unique(append(machine.Tags, addTag[i]))
+		}
+	}
+
+	if c.String("remove-tag") != "" {
+		machine.Tags = difference(machine.Tags, separateTags(c.String("remove-tag")))
+	}
+
 	return nil
 }
--- machine.go
+++ machine.go
@@ -5,13 +5,13 @@ import (
 )
 
 // Flag are going to be used by providers, orchestrators and other tooling.
-type Flag string
+type Tag string
 
 // This is the bare minimum to represent a computer.
 type Machine struct {
 	Ip net.IP
 	Domain string
-	Flags []Flag
+	Tags []Tag
 }
 
 // This is the bare minimum actions a machine can perform.
